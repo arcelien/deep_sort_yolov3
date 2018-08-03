0db144161ae597c99421d1db3aa2dc6fa5508224
Daniel Ho <danho@nvidia.com>
2018-08-03 12:59:24 -0700

    test only yolo, not deep sort

--- demo-dataset.py
+++ demo-dataset.py
@@ -30,9 +30,9 @@ def main(yolo):
     
    # deep_sort 
     model_filename = 'model_data/mars-small128.pb'
-    encoder = gdet.create_box_encoder(model_filename,batch_size=1)
-    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
-    tracker = Tracker(metric)
+    # encoder = gdet.create_box_encoder(model_filename,batch_size=1)
+    # metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    # tracker = Tracker(metric)
     
     fps = 0.0
     index = 0
@@ -56,30 +56,30 @@ def main(yolo):
         print("box_num",len(boxs))
         print("boxes", boxs)
 
-        features = encoder(frame,boxs)
-        # score to 1.0 here).
-        detections = [Detection(bbox, 1.0, feature) for bbox, feature in zip(boxs, features)]
+        # features = encoder(frame,boxs)
+        # # score to 1.0 here).
+        # detections = [Detection(bbox, 1.0, feature) for bbox, feature in zip(boxs, features)]
         
-        # Run non-maxima suppression.
-        boxes = np.array([d.tlwh for d in detections])
-        scores = np.array([d.confidence for d in detections])
-        indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
-        detections = [detections[i] for i in indices]
+        # # Run non-maxima suppression.
+        # boxes = np.array([d.tlwh for d in detections])
+        # scores = np.array([d.confidence for d in detections])
+        # indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
+        # detections = [detections[i] for i in indices]
         
-        # Call the tracker
-        tracker.predict()
-        tracker.update(detections)
+        # # Call the tracker
+        # tracker.predict()
+        # tracker.update(detections)
         
-        for track in tracker.tracks:
-            if track.is_confirmed() and track.time_since_update >1 :
-                continue 
-            bbox = track.to_tlbr()
-            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
-            cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
+        # for track in tracker.tracks:
+        #     if track.is_confirmed() and track.time_since_update >1 :
+        #         continue 
+        #     bbox = track.to_tlbr()
+        #     cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
+        #     cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
 
-        for det in detections:
-            bbox = det.to_tlbr()
-            cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
+        # for det in detections:
+        #     bbox = det.to_tlbr()
+        #     cv2.rectangle(frame,(int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,0,0), 2)
             
         # cv2.imshow('', frame)
             
