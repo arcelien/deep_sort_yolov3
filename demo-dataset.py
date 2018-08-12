828a994e5f76e5fbf9ad47c7c377832c38b7c205
Daniel Ho <danho@nvidia.com>
2018-08-11 22:22:52 -0700

    making test case for tracker

--- demo-dataset.py
+++ demo-dataset.py
@@ -30,15 +30,22 @@ def main(yolo):
     
    # deep_sort 
     model_filename = 'model_data/mars-small128.pb'
-    # encoder = gdet.create_box_encoder(model_filename,batch_size=1)
-    # metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
-    # tracker = Tracker(metric)
+    encoder = gdet.create_box_encoder(model_filename,batch_size=1)
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    tracker = Tracker(metric)
     
     fps = 0.0
     index = 0
     image_name = "isaac-dataset/image_"
+    
+    # save for testing tracker: 
+    # input: detections for each frame = [tlwh, confidence, feature]
+    # output: list of bboxes for tracks
+    
+    saved_inputs = []
+    saved_outputs = []
     while True:
-        if index < 13 + 3:
+        if index < 3:
             index += 1
             continue
 		
@@ -56,24 +63,32 @@ def main(yolo):
         print("box_num",len(boxs))
         print("boxes", boxs)
 
-        # features = encoder(frame,boxs)
+        features = encoder(frame,boxs)
         # # score to 1.0 here).
-        # detections = [Detection(bbox, 1.0, feature) for bbox, feature in zip(boxs, features)]
+        detections = [Detection(bbox, 1.0, feature) for bbox, feature in zip(boxs, features)]
         
         # # Run non-maxima suppression.
-        # boxes = np.array([d.tlwh for d in detections])
-        # scores = np.array([d.confidence for d in detections])
-        # indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
-        # detections = [detections[i] for i in indices]
+        boxes = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
+        detections = [detections[i] for i in indices]
         
+        # save inputs
+        saved_inputs.append([(d.tlwh, d.confidence, d.feature) for d in detections])
+
         # # Call the tracker
-        # tracker.predict()
-        # tracker.update(detections)
+        tracker.predict()
+        tracker.update(detections)
         
-        # for track in tracker.tracks:
-        #     if track.is_confirmed() and track.time_since_update >1 :
-        #         continue 
-        #     bbox = track.to_tlbr()
+        one_output = []
+
+        for track in tracker.tracks:
+            if track.is_confirmed() and track.time_since_update >1 :
+                continue 
+            bbox = track.to_tlbr()
+            one_output.append(bbox)
+
+        saved_outputs.append(one_output)
         #     cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])),(255,255,255), 2)
         #     cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1])),0, 5e-3 * 200, (0,255,0),2)
 
@@ -91,6 +106,12 @@ def main(yolo):
             break
 
         # time.sleep(0.1)
+
+    print("end, showing saved in/out")
+    print(saved_inputs[0])
+    print(saved_outputs[0])
+    
+    
 
 if __name__ == '__main__':
     main(YOLO())
